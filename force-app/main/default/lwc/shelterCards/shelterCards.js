b43464756e42c27bbef99787aceafbb9a1d4e8e4
lostbtw <bozo@>
2026-06-02 15:19:16 +0200

    feat add the rest of the info about shelters to shelter dashboard

--- force-app/main/default/lwc/shelterCards/shelterCards.js
+++ force-app/main/default/lwc/shelterCards/shelterCards.js
@@ -7,10 +7,40 @@ export default class ShelterCards extends LightningElement {
     @wire(getShelters)
     wiredShelters({ data, error }) {
         if (data) {
-            this.shelters = data;
-            console.log('SHELTERS:', data);
+            this.shelters = data.map(s => {
+                return {
+                    ...s,
+
+                    mon: this.formatDay(s.Monday_Open__c, s.Monday_Close__c),
+                    tue: this.formatDay(s.Tuesday_Open__c, s.Tuesday_Close__c),
+                    wed: this.formatDay(s.Wednesday_Open__c, s.Wednesday_Close__c),
+                    thu: this.formatDay(s.Thursday_Open__c, s.Thursday_Close__c),
+                    fri: this.formatDay(s.Friday_Open__c, s.Friday_Close__c),
+                    sat: this.formatDay(s.Saturday_Open__c, s.Saturday_Close__c),
+                    sun: this.formatDay(s.Sunday_Open__c, s.Sunday_Close__c)
+                };
+            });
         } else if (error) {
-            console.error(error);
+            console.error('ERROR:', error);
         }
+    }
+
+    formatDay(open, close) {
+        if (!open || !close) {
+            return 'Closed';
+        }
+
+        return `${this.formatTime(open)} - ${this.formatTime(close)}`;
+    }
+
+    formatTime(ms) {
+        if (ms === null || ms === undefined) return '';
+
+        const date = new Date(ms);
+
+        return date.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit'
+        });
     }
 }
