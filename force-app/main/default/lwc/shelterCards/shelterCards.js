65cbc2b8f6bf9a13535e7d21e1f8678b8436e517
lostbtw <bozo@>
2026-06-02 14:47:29 +0200

    feat implement basic lwc component which displays names of shelters

--- force-app/main/default/lwc/shelterCards/shelterCards.js
+++ force-app/main/default/lwc/shelterCards/shelterCards.js
@@ -1,3 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getShelters from '@salesforce/apex/ShelterController.getShelters';
 
-export default class ShelterCards extends LightningElement {}
+export default class ShelterCards extends LightningElement {
+    shelters = [];
+
+    @wire(getShelters)
+    wiredShelters({ data, error }) {
+        if (data) {
+            this.shelters = data;
+            console.log('SHELTERS:', data);
+        } else if (error) {
+            console.error(error);
+        }
+    }
+}
