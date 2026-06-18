f1049b20f75758f8805bad55e0593b49914da631
lostbtw <almazyurusov@gmail.com>
2026-06-18 11:28:26 +0200

    feat connect the put endpoint

--- force-app/main/default/lwc/missingAnimalsList/missingAnimalsList.js
+++ force-app/main/default/lwc/missingAnimalsList/missingAnimalsList.js
@@ -1,12 +1,17 @@
 import { LightningElement, track } from 'lwc';
-import getAnimalsWithImages from '@salesforce/apex/AnimalController.getAnimalsWithImages';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getAnimalsWithImages from '@salesforce/apex/AnimalController.getAnimalsWithImages';
+import updateFoundStatus from '@salesforce/apex/AnimalController.updateFoundStatus';
 
 export default class MissingAnimalsList extends LightningElement {
-    dayDelta;
     @track animals = [];
-    hasSearched = false;
+    dayDelta;
     isLoading = false;
+    hasSearched = false;
+
+    isModalOpen = false;
+    selectedAnimalId;
+    selectedAnimalName;
 
     handleDayDeltaChange(event) {
         this.dayDelta = event.target.value;
@@ -37,12 +42,46 @@ export default class MissingAnimalsList extends LightningElement {
     }
 
     showToast(title, message, variant) {
-        this.dispatchEvent(
-            new ShowToastEvent({
-                title: title,
-                message: message,
-                variant: variant
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+        this.dispatchEvent(event);
+    }
+
+    handleAnimalClick(event) {
+        this.selectedAnimalId = event.currentTarget.dataset.id;
+        const animal = this.animals.find(a => a.id === this.selectedAnimalId);
+        this.selectedAnimalName = animal ? animal.name : this.selectedAnimalId;
+        this.isModalOpen = true;
+    }
+
+    closeModal() {
+        this.isModalOpen = false;
+        this.selectedAnimalId = null;
+        this.selectedAnimalName = null;
+    }
+
+    confirmFound() {
+        this.isLoading = true;
+        this.isModalOpen = false;
+        
+        updateFoundStatus({ animalId: this.selectedAnimalId, found: true })
+            .then(() => {
+                this.showToast('Success', `${this.selectedAnimalName} has been marked as found.`, 'success');
+                this.animals = this.animals.filter(a => a.id !== this.selectedAnimalId);
             })
-        );
+            .catch(error => {
+                console.error('Update error', error);
+                let errorMessage = 'An error occurred while updating status.';
+                if (error && error.body && error.body.message) {
+                    errorMessage = error.body.message;
+                }
+                this.showToast('Error', errorMessage, 'error');
+            })
+            .finally(() => {
+                this.isLoading = false;
+            });
     }
 }
