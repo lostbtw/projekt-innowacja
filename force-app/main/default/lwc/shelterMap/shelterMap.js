f7622321af44647c6bccd073bd7efb8c0f18f363
B1ona4y <eu.oleynich@gmail.com>
2026-06-18 19:42:24 +0200

    style: formating issues fix

--- force-app/main/default/lwc/shelterMap/shelterMap.js
+++ force-app/main/default/lwc/shelterMap/shelterMap.js
@@ -1,46 +1,46 @@
-import { LightningElement, api, wire } from "lwc";
-import getSheltersInSameCity from "@salesforce/apex/ShelterMapController.getSheltersInSameCity";
+import { LightningElement, api, wire } from 'lwc';
+import getSheltersInSameCity from '@salesforce/apex/ShelterMapController.getSheltersInSameCity';
 
 export default class ShelterMap extends LightningElement {
-  @api recordId;
+    @api recordId;
 
-  mapMarkers = [];
-  selectedMarkerValue = "";
-  error;
-  zoomLevel = 12;
+    mapMarkers = [];
+    selectedMarkerValue = '';
+    error;
+    zoomLevel = 12;
 
-  @wire(getSheltersInSameCity, { shelterId: "$recordId" })
-  wiredShelters({ error, data }) {
-    if (data) {
-      this.error = undefined;
-      this.buildMarkers(data);
-    } else if (error) {
-      this.error = error.body?.message || "Failed to load shelters.";
-      this.mapMarkers = [];
+    @wire(getSheltersInSameCity, { shelterId: '$recordId' })
+    wiredShelters({ error, data }) {
+        if (data) {
+            this.error = undefined;
+            this.buildMarkers(data);
+        } else if (error) {
+            this.error = error.body?.message || 'Failed to load shelters.';
+            this.mapMarkers = [];
+        }
     }
-  }
 
-  buildMarkers(shelters) {
-    this.mapMarkers = shelters.map((shelter) => ({
-      location: {
-        Street: shelter.Address__Street__s || "",
-        City: shelter.Address__City__s || "",
-        State: shelter.Address__StateCode__s || "",
-        PostalCode: shelter.Address__PostalCode__s || "",
-        Country: shelter.Address__CountryCode__s || ""
-      },
-      value: shelter.Id,
-      title: shelter.Name,
-      description: `${shelter.Address__Street__s || ""}, ${shelter.Address__City__s || ""}`,
-      icon: "standard:account"
-    }));
-  }
+    buildMarkers(shelters) {
+        this.mapMarkers = shelters.map(shelter => ({
+            location: {
+                Street: shelter.Address__Street__s || '',
+                City: shelter.Address__City__s || '',
+                State: shelter.Address__StateCode__s || '',
+                PostalCode: shelter.Address__PostalCode__s || '',
+                Country: shelter.Address__CountryCode__s || ''
+            },
+            value: shelter.Id,
+            title: shelter.Name,
+            description: `${shelter.Address__Street__s || ''}, ${shelter.Address__City__s || ''}`,
+            icon: 'standard:account'
+        }));
+    }
 
-  get hasMarkers() {
-    return this.mapMarkers.length > 0;
-  }
+    get hasMarkers() {
+        return this.mapMarkers.length > 0;
+    }
 
-  get cardTitle() {
-    return `Shelters in the same city (${this.mapMarkers.length})`;
-  }
+    get cardTitle() {
+        return `Shelters in the same city (${this.mapMarkers.length})`;
+    }
 }
